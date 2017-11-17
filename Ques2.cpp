0067f422f4a044fba48026b0b8812317b58bcb07
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-11-18 00:08:35 +0530

    Update Ques2.cpp

--- Ques2.cpp
+++ Ques2.cpp
@@ -1,143 +1,135 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int ar[5];
-int top = -1;
-void push(int num);
-void pop();
-void display();
-void push(int num)
-{
-  if(top == 4)
-  {
-   cout<<"\n   Stack is full or overflow! \n";
-  }
-  else
-  {
-     top++;
-     ar[top] = num;
-  }
-}
-void pop()
-{
-  if(top == -1)
-  {
-      cout<<"\n Stack is Empty or underflow! \n";
-  }
-  else
-  {
-      top--;
-      cout<<"Deleted number is : ";
-      cout<<ar[top];
-  }
-}
-void display()
-{
-  int top;
-  if(top == -1)
-  {
-     cout<<"Array is Empty or underflow.\n";
-  }
 
-     else
-     {
-       cout<<"\n Array is : ";
-      while(top!=-1)
-      {
-          cout<<"    "<<ar[top];
-          top--;
-      }
-     }
-}
-class node
-{
-  public:
-   int data;
-   node*next;
-   node*top;
-   node()
-   {
-       top = NULL;
-   }
-void push(int x)
-{
-   node *head;
-   head = new node;
-   head->data;
-   head->next = NULL;
-   if(top!=NULL)
-   {
-        head->next=top;
-        top = head;
-   }
-}
-void pop()
-{
-  node*temp;
-  if(top==NULL)
-  {
-      cout<<"\n The stack is empty!!!";
-      return;
-  }
-  temp=top;
-  top=top->next;
-  delete temp;
-}
-void display()
-{
-    node *ptr=top;
-    while(ptr!=NULL)
-    {
-        cout<<ptr->data<<endl;
-        ptr=ptr->next;
-    }
-    cout<<endl;
-}
+class Node {
+	public:
+		int data;
+		Node* next;
+
+		Node() {
+			data = 0;
+			next = NULL;
+		}
 };
-int main()
-{
-   char ch,ch1;
-   int num;
-   node n;
-   cout<<"1.Stack using array \n 2.Stack using linklist\n";
-   cin>>ch1;
-   switch(ch1)
-   {
-   case '1':
-   do
-   {
-       cout<<"\n 1.Push \n 2.Pop \n 3.Display \n 4.Exit : \n";
-       cin>>ch;
-       switch(ch)
-       {
-           case '1' : cout<<"Enter a value : ";
-                      cin>>num;
-                      push(num);
-                      break;
-           case '2' : pop();
-                      break;
-           case '3' : display();
-                      break;
-       }
-   }while(ch!='4'); break;
-   case '2':
-    do
-   {
-       cout<<"\n 1.Push \n 2.Pop \n 3.Display \n 4.Exit : \n";
-       cin>>ch;
-       switch(ch)
-       {
-           case '1' : cout<<"Enter a value : ";
-                      cin>>num;
-                      n.push(num);
-                      break;
-           case '2' : n.pop();
-                      break;
-           case '3' : n.display();
-                      break;
-       }
-   }while(ch!='4'); break;
-   default: cout<<"wrong input";
-   }
 
-   return 0;
+class Stack {
+	private:
+		Node* head;
+		Node* tail;
+		int n;
+
+	public:
+		Stack() {
+			head = NULL;
+			tail = NULL;
+			n = 0;
+		}
+
+		void push(int x);
+		void pop();
+		void display();
+};
+
+void Stack::push(int x) {
+	Node* ptemp = new Node;
+	ptemp->data = x;
+	ptemp->next = NULL;
+
+	if (head == NULL) {
+		head = ptemp;
+		tail = ptemp;
+		++n;
+	} else {
+		tail->next = ptemp;
+		tail = ptemp;
+		++n;
+	}
+}
+
+void Stack::pop() {
+	Node* move = head;
+	for (int j=0; j<n-2; ++j)
+		move=move->next;
+
+	move->next = NULL;
+	tail = move;
+	--n;
+}
+
+void Stack::display() {
+	if (head == NULL) {
+		cout << "There are no elements in the stack." << endl;
+	} else {
+		Node* move = head;
+		while(move != NULL) {
+			cout << move->data << endl;
+			move = move->next;
+		}
+	}
+}
+
+
+
+
+
+
+int length(int* array) {
+	int i;
+	for (i=0; *(array+i)!='\0'; ++i) {}
+
+	return i;
+}
+
+void initialize(int* array, int size) {
+	for (int i=0; i<size; ++i) {
+		*(array+i) = '\0';
+	}
+}
+
+void push(int* array, int x) {
+	*(array+length(array)) = x;
+}
+
+void pop(int* array) {
+	*(array+length(array)-1) = '\0';
+}
+
+void display(int* array) {
+	for (int i=0; i<length(array); ++i) {
+		cout << *(array+i) << endl;
+	}
+}
+
+int main() {
+	/*Implementation as an array*/
+  int var[50];
+	initialize(var, 50);
+
+	cout << length(var) << endl;
+	cout << "Enter five values of stack: " << endl;
+	int fi=0;
+	for (int i=0; i<5; ++i) {
+		cin >> fi;
+		push(var, fi);
+	}
+	pop(var);
+	display(var);
+  
+  /*Implementation as a Linked list*/
+  Stack stack1;
+	int var=0;
+	cout << "Enter five elements to the list: " << endl;
+	for (int i=0; i<5; ++i) {
+		cin >> var;
+		stack1.push(var);
+	}
+
+	stack1.display();
+	stack1.push(25);
+	stack1.display();
+	stack1.pop();
+	stack1.display();
+
+
+	return 0;
 }
